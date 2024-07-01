eefa84ca113a9ecfcbf004129ff2be914966f646
Manuel Zedel <manuel.zedel@northern.tech>
2024-07-01 14:59:07 +0200

    chore: debug
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/e2e_tests/playwright.config.ts
+++ tests/e2e_tests/playwright.config.ts
@@ -13,6 +13,10 @@
 //    limitations under the License.
 import { LaunchOptions, PlaywrightTestConfig } from '@playwright/test';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const contextArgs = {
   acceptDownloads: true,
